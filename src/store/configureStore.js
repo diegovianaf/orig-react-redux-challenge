70bffb714bdff80d44b47b209c8cc174d1e4a467
Diego Viana <diegovianaf7@gmail.com>
2022-04-28 14:05:26 -0300

    first commit

---
+++ src/store/configureStore.js
@@ -0,0 +1,14 @@
+import {
+  combineReducers,
+  configureStore,
+  getDefaultMiddleware,
+} from '@reduxjs/toolkit'
+import login from './login'
+import localStorage from './middleware/localStorage'
+
+const middleware = [...getDefaultMiddleware(), localStorage]
+
+const reducer = combineReducers({ login })
+const store = configureStore({ reducer, middleware })
+
+export default store
